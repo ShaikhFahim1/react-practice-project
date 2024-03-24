a9fcaecf954387901968d8e26891cf9a62439e68
ShaikhFahim1 <sf792914@gmail.com>
2024-03-24 19:46:01 +0530

    Initial

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    if (count <= 5) {
+      console.log(count);
+      count = count + 1;
+      setCount(count);
+    } else {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={handleIncrement}>Add count is {count}</button>
+      </div>
+    </>
+  );
+}
+
+export default App;
